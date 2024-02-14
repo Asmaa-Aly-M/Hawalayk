ba401b0c3a72aa74ccbb86bac73c50e8cf09b45a
esraa_hay <esraamahmoud87870@gmail.com>
2024-02-15 00:22:41 +0200

    appreport

---
+++ Hawalayk APP/model/AppReport.cs
@@ -0,0 +1,11 @@
+﻿namespace Hawalayk_APP.model
+{
+    public class AppReport
+    {
+        public int Id { get; set; }
+        public User Reporter { get; set; }
+        public ReportedIssue ReportedIssue { get; set; }
+        public string Description { get; set; }
+        public DateTime DatePosted { get; set; }
+    }
+}
