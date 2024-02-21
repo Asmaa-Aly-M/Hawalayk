e0aa8b9b7bbb0af193dfef505192311c6455c852
Isra Abdelmobdy <progisra491@gmail.com>
2024-02-21 21:03:53 +0200

    Authentication

--- Hawalayk APP/Models/RegisterCustomerModel.cs
+++ Hawalayk APP/Models/RegisterCustomerModel.cs
@@ -4,19 +4,19 @@ namespace Hawalayk_APP.Models
 {
     public class RegisterCustomerModel
     {
-        [Required, StringLength(100)]
+        [Required, StringLength(50)]
         public string FirstName { get; set; }
 
-        [Required, StringLength(100)]
+        [Required, StringLength(50)]
         public string LastName { get; set; }
 
-        [Required, StringLength(50)]
+        [Required, StringLength(30)]
         public string Username { get; set; }
 
-        [Required, StringLength(128)]
+        [Required, StringLength(11)]
         public string PhoneNumber { get; set; }
 
-        [Required, StringLength(256)]
+        [Required, StringLength(50)]
         public string Password { get; set; }
 
         public DateTime BirthDate { get; set; }
--- Hawalayk APP/Services/AppReportRepository.cs
+++ Hawalayk APP/Services/AppReportRepository.cs
@@ -1,56 +1,56 @@
-﻿//using Hawalayk_APP.Context;
-//using Hawalayk_APP.Models;
+﻿using Hawalayk_APP.Context;
+using Hawalayk_APP.Models;
 
-//namespace Hawalayk_APP.Services
-//{
-//    public class AppReportRepository
-//    {
-
-
-//        ApplicationDbContext _context;
-//        public CustomerRepository(ApplicationDbContext context)
-//        {
-//            _context = context;
-
-//        }
-
-//        public List<AppReport> GetAll()
-//        {
-//            List<AppReport> AppReports = _context.Customers.ToList();
-
-//            return AppReports;
+namespace Hawalayk_APP.Services
+{
+    public class AppReportRepository
+    {
 
 
-//        }
-//        public AppReport GetById(string id)
-//        {
-//            AppReport appreport = _context.AppReports.SingleOrDefault(c => c.Id == id);
-//            return appreport;
-//        }
-//        public int Create(AppReport appreport)
-//        {
-//            _context.AppReports.Add(appreport);
-//            int row = _context.SaveChanges();
-//            return row;
-//        }
-//        public int Update(string id, AppReport appreport)
-//        {
-//            AppReport Oldappreport = _context.AppReports.SingleOrDefault(c => c.Id == id);
-//            Oldappreport.Id = id;
-//            Oldappreport.Reporter = appreport.Reporter;
-//            Oldappreport.ReporerId = appreport.ReporerId;
-//            Oldappreport.ReportedIssue = appreport.ReportedIssue;
-//            Oldappreport.DatePosted = appreport.DatePosted;
-//            Oldappreport.Description = appreport.Description;
+        ApplicationDbContext _context;
+        public CustomerRepository(ApplicationDbContext context)
+        {
+            _context = context;
 
-//            int row = _context.SaveChanges();
-//            return row;
-//        }
-//        public int Delete(Customer customer)
-//        {
-//            _context.Customers.Remove(customer);
-//            int row = _context.SaveChanges();
-//            return row;
-//        }
-//    }
-//}
+        }
+
+        public List<AppReport> GetAll()
+        {
+            List<AppReport> AppReports = _context.Customers.ToList();
+
+            return AppReports;
+
+
+        }
+        public AppReport GetById(string id)
+        {
+            AppReport appreport = _context.AppReports.SingleOrDefault(c => c.Id == id);
+            return appreport;
+        }
+        public int Create(AppReport appreport)
+        {
+            _context.AppReports.Add(appreport);
+            int row = _context.SaveChanges();
+            return row;
+        }
+        public int Update(string id, AppReport appreport)
+        {
+            AppReport Oldappreport = _context.AppReports.SingleOrDefault(c => c.Id == id);
+            Oldappreport.Id = id;
+            Oldappreport.Reporter = appreport.Reporter;
+            Oldappreport.ReporerId = appreport.ReporerId;
+            Oldappreport.ReportedIssue = appreport.ReportedIssue;
+            Oldappreport.DatePosted = appreport.DatePosted;
+            Oldappreport.Description = appreport.Description;
+
+            int row = _context.SaveChanges();
+            return row;
+        }
+        public int Delete(Customer customer)
+        {
+            _context.Customers.Remove(customer);
+            int row = _context.SaveChanges();
+            return row;
+        }
+    }
+}
