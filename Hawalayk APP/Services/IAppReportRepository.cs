23998802222de2566df5e90bc2141cda5a7fe15d
esraa_hay <esraamahmoud87870@gmail.com>
2024-02-22 22:51:59 +0200

    advertisement controller

--- Hawalayk APP/Services/IAppReportRepository.cs
+++ Hawalayk APP/Services/IAppReportRepository.cs
@@ -5,7 +5,7 @@ namespace Hawalayk_APP.Services
     public interface IAppReportRepository
     {
         int Create(AppReport appreport);
-        int Delete(Customer customer);
+        int Delete(AppReport appreport);
         List<AppReport> GetAll();
         AppReport GetById(int id);
         int Update(int id, AppReport appreport);
