19c2b7301a9409134dcd6df52925358010103f57
esraa_hay <esraamahmoud87870@gmail.com>
2024-02-28 21:36:42 +0200

    update and delete post

---
+++ Hawalayk APP/Controllers/PostController.cs
@@ -0,0 +1,37 @@
+﻿using Hawalayk_APP.Models;
+using Hawalayk_APP.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Hawalayk_APP.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PostController : ControllerBase
+    {
+        IPostRepository postrepository;
+        PostController(IPostRepository _postrepository) 
+        {
+            postrepository= _postrepository;
+        }
+        [HttpPut]
+        public IActionResult Update(int id,Post post)
+        {
+            Post oldpost = postrepository.GetById(id);
+            postrepository.Update(id, oldpost);
+            return Ok();
+            
+        }
+        [HttpDelete]
+        public IActionResult Delete(int id)
+        {
+            Post oldpost = postrepository.GetById(id);
+            if (oldpost == null)
+            {
+               return NotFound();
+            }
+            postrepository.Delete(id);
+            return NoContent();
+        }
+    }
+}
