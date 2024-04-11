c40b32c69765dc8291cd22ceba86fd11c9558235
esraa_hay <esraamahmoud87870@gmail.com>
2024-04-11 17:39:24 +0200

    review hub and post hub

---
+++ Hawalayk APP/System Hub/PostHub.cs
@@ -0,0 +1,14 @@
+﻿using Hawalayk_APP.Models;
+using Microsoft.AspNetCore.SignalR;
+
+namespace Hawalayk_APP.System_Hub
+{
+    public class PostHub:Hub
+    {
+        public async Task SendReviewAsync(Post post)
+        {
+            // Broadcast the new review to all connected clients
+            await Clients.All.SendAsync("ReceivePost", post);
+        }
+    }
+}
---
+++ Hawalayk APP/System Hub/ReviewHub.cs
@@ -0,0 +1,14 @@
+﻿using Hawalayk_APP.Models;
+using Microsoft.AspNetCore.SignalR;
+
+namespace Hawalayk_APP.System_Hub
+{
+    public class ReviewHub :  Hub
+    {
+        public async Task SendReviewAsync(Review reviewdata)
+        {
+            // Broadcast the new post to all connected clients
+            await Clients.All.SendAsync("ReceiveReview", reviewdata);
+        }
+    }
+}
