d084ce967c812889992558f9d2ebfc7eeda8e8ba
Isra Abdelmobdy <progisra491@gmail.com>
2024-04-25 11:53:25 +0200

    Address,,Governorate,City

--- Hawalayk APP/Program.cs
+++ Hawalayk APP/Program.cs
@@ -39,7 +39,7 @@ namespace Hawalayk_APP
             builder.Services.AddScoped<IAuthService, AuthService>();
             builder.Services.AddScoped<IPostRepository, PostRepository>();
             builder.Services.AddScoped<IAdvertisementRepository, AdvertisementRepository>();
-            builder.Services.AddScoped<IAppReportRepository, AppReportRepository>();/////
+            builder.Services.AddScoped<IAppReportRepository, AppReportRepository>();
             builder.Services.AddScoped<IReviewRepository, ReviewRepository>();
             builder.Services.AddScoped<IUserReportRepository, UserReportRepository>();
             builder.Services.AddScoped<IServiceRequestRepository, ServiceRequestRepository>();
@@ -48,52 +48,59 @@ namespace Hawalayk_APP
             builder.Services.AddScoped<ISeedingDataService, SeedingDataService>();
             #endregion
 
-
-
-
-
             builder.Services.AddIdentity<ApplicationUser, IdentityRole>()
                 .AddEntityFrameworkStores<ApplicationDbContext>();
 
             builder.Services.AddDbContext<ApplicationDbContext>(options =>
-            options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
-
+                options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
             builder.Services.AddAuthentication(options =>
             {
                 options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
                 options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
             })
-                .AddJwtBearer(o =>
+            .AddJwtBearer(options =>
+            {
+                options.RequireHttpsMetadata = false;
+                options.SaveToken = true;
+                options.TokenValidationParameters = new TokenValidationParameters
                 {
-                    o.RequireHttpsMetadata = false;
-                    o.SaveToken = false;
-                    o.TokenValidationParameters = new TokenValidationParameters
-                    {
-                        ValidateIssuerSigningKey = true,
-                        ValidateIssuer = true,
-                        ValidateAudience = true,
-                        ValidateLifetime = true,
-                        ValidIssuer = builder.Configuration["JWT:Issuer"],
-                        ValidAudience = builder.Configuration["JWT:Audience"],
-                        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["JWT:Key"]))
-                    };
-                });
+                    ValidateIssuer = true,
+                    ValidateAudience = true,
+                    ValidateLifetime = true,
+                    ValidateIssuerSigningKey = true,
+                    ValidIssuer = builder.Configuration["JWT:Issuer"],
+                    ValidAudience = builder.Configuration["JWT:Audience"],
+                    IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["JWT:Key"]))
+                };
+            });
 
             builder.Services.AddCors();
-            builder.Services.AddSignalR();///////////// ؟؟؟؟مكانها صح
-
-
-
+            builder.Services.AddSignalR();
 
             builder.Services.AddControllers();
-            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
+
             builder.Services.AddSwaggerGen();
             builder.Services.AddScoped<DataSeeder>();
             var seeder = builder.Services.BuildServiceProvider().GetRequiredService<DataSeeder>();
             seeder.SeedGovernoratesData(builder.Services.BuildServiceProvider().GetRequiredService<ApplicationDbContext>());
             //seeder.SeedCitiesData(builder.Services.BuildServiceProvider().GetRequiredService<ApplicationDbContext>());
+
+            builder.Services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new Microsoft.OpenApi.Models.OpenApiInfo { Title = "Your API", Version = "v1" });
+                c.OperationFilter<SecurityRequirementsOperationFilter>();
+                c.AddSecurityDefinition("Bearer", new Microsoft.OpenApi.Models.OpenApiSecurityScheme
+                {
+                    Description = "JWT Authorization header using the Bearer scheme. Example: \"Bearer {token}\"",
+                    Name = "Authorization",
+                    In = Microsoft.OpenApi.Models.ParameterLocation.Header,
+                    Type = Microsoft.OpenApi.Models.SecuritySchemeType.ApiKey,
+                    Scheme = "Bearer"
+                });
+            });
+
 
             var app = builder.Build();
             
@@ -111,30 +118,43 @@ namespace Hawalayk_APP
             }
             #endregion
 
-
-            // Configure the HTTP request pipeline.
             if (app.Environment.IsDevelopment())
             {
                 app.UseSwagger();
-                app.UseSwaggerUI();
+                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "Your API V1"));
             }
 
-            app.UseCors(c => c
-            .AllowAnyHeader()
-            .AllowAnyMethod()
-            .AllowAnyOrigin()
-
-
-            );
+            app.UseCors(c => c.AllowAnyHeader().AllowAnyMethod().AllowAnyOrigin());
 
             app.UseAuthentication();
             app.UseAuthorization();
 
-
             app.MapControllers();
 
-
             app.Run();
+        }
+    }
+
+    public class SecurityRequirementsOperationFilter : Swashbuckle.AspNetCore.SwaggerGen.IOperationFilter
+    {
+        public void Apply(Microsoft.OpenApi.Models.OpenApiOperation operation, Swashbuckle.AspNetCore.SwaggerGen.OperationFilterContext context)
+        {
+            if (operation.Security == null)
+                operation.Security = new System.Collections.Generic.List<Microsoft.OpenApi.Models.OpenApiSecurityRequirement>();
+
+            var scheme = new Microsoft.OpenApi.Models.OpenApiSecurityScheme
+            {
+                Reference = new Microsoft.OpenApi.Models.OpenApiReference
+                {
+                    Type = Microsoft.OpenApi.Models.ReferenceType.SecurityScheme,
+                    Id = "Bearer"
+                }
+            };
+
+            operation.Security.Add(new Microsoft.OpenApi.Models.OpenApiSecurityRequirement
+            {
+                [scheme] = new System.Collections.Generic.List<string>()
+            });
         }
     }
 }
