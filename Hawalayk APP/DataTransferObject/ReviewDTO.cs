27c68d1993d377eee2dd232c3420b1ed436779cf
Asmaa-Aly-M <asmaa.ali.m.e@gmail.com>
2024-06-15 22:41:38 +0200

    remove ID

--- Hawalayk APP/DataTransferObject/ReviewDTO.cs
+++ Hawalayk APP/DataTransferObject/ReviewDTO.cs
@@ -3,10 +3,7 @@
 namespace Hawalayk_APP.DataTransferObject
 {
     public class ReviewDTO
-    {//ايه الي محتاجينه بالظبط في ال dto?
-    
-        public int Id { get; set; }
-
+    {
         [Range(1, 5, ErrorMessage = "Rating must be between 1 and 5.")]
         public int Rating { get; set; }
         [Required(ErrorMessage = "Content is required.")]
